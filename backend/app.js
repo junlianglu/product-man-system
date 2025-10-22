18743ddb5eeae2d26119fa36f54145527200dec8
Junyu Lu <141600678+junyulu463@users.noreply.github.com>
2025-10-22 16:40:00 -0700

    fix the local picture update problem

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,14 @@ import userRoutes from './routes/authRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import cors from "cors"
 
+import uploadRoutes from "./routes/uploadRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -15,6 +22,9 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/api/cart', cartRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); 
+app.use("/api/upload", uploadRoutes);
 
 export default app;
 
